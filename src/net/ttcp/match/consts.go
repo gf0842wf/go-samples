c045158bbc74b7963f937b748019bd28b8276b24
gf0842wf <gf0842wf@gmail.com>
2014-06-23 23:46:09 +0800

    m
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- src/net/ttcp/match/consts.go
+++ src/net/ttcp/match/consts.go
@@ -1,5 +1,8 @@
 package match
 
 const (
-	DESK_SIZE int = 30 // 30 个桌吧
+	DESK_SIZE     int  = 30    // 30 个桌吧
+	MAX_MEMBERS   int  = 6     // 最大人数
+	MIN_MEMBERS   int  = 2     // 最小人数
+	HALFWAY_ENTER bool = false // 是否允许中途进入
 )
--- src/net/ttcp/match/match.go
+++ src/net/ttcp/match/match.go
@@ -4,6 +4,7 @@ package match
 
 import (
 	"fmt"
+	"time"
 )
 
 import (
@@ -11,38 +12,90 @@ import (
 	"zmap"
 )
 
-var Desks *zmap.SafeMap
-
+// 这个Desk类可以继承
 type Desk struct {
+	Status    int // 0-未开设, 1-游戏中, 2-结束游戏
+	GameID    uint32
+	StartTime time.Time
+	EndTime   time.Time
 
+	MQ    chan string
+	Users [MAX_MEMBERS]*types.User // 数组很小,用值拷贝没问题
 }
 
-type Match struct {
-	chan MQ string
+func (d *Desk) Add(user *types.User) bool {
+	for i, v := range d.Users {
+		if v == nil {
+			d.Users[i] = user
+			return true
+		}
+	}
+	return false
 }
 
-func (m *Match) Start() {
+func (d *Dsek) Remove(user *types.User) bool {
+	for i, v := range d.Users {
+		if v == user {
+			d.Users[i] = nil
+			return true
+		}
+	}
+	return false
+}
+
+func (d *Desk) Delete(uid uint32) bool {
+	user_ := types.Users.Get(uid)
+	if user_ != nil {
+		return d.Remove(user_.(*types.User))
+	}
+	return false
+}
+
+// 空闲座位数
+func (d *Desk) EptPos() (cnt int) {
+	cnt = 0
+	for i, v := range d.Users {
+		if v == nil {
+			cnt++
+		}
+	}
+	return
+}
+
+func NewDesk() *Desk {
+	ret = &Desk{}
+	for i := 0; i < MAX_MEMBERS; i++ {
+		ret.Users[i] = nil
+	}
+	return ret
+}
+
+var Desks [DESK_SIZE]*Desk
+
+func (d *Desk) Start() {
 	defer func() {
 		if x := recover(); x != nil {
-			fmt.Println("Caught panic in match goroutine")
+			fmt.Println("Caught panic in desk goroutine")
 			panic(x)
 		}
 	}()
 
 	for {
 		select {
-		case data := <-m.MQ:
+		case data := <-d.MQ:
 			fmt.Println(data)
+		}
 	}
 }
 
 // 随机分配
 // 调用这个函数要加锁
-// TODO: 返回一个desk,在同一个桌里的user返回同一个desk
-func (m *Match)RandAlloc(user *types.User) {
+func RandAlloc(user *types.User) {
+	for num, desk := range Desks {
 
+	}
 }
 
 func init() {
-	Desks = zmap.NewSafeMap()
+
 }
