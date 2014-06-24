03e11c3a685d0810f7f60317f5ead26760d9b74e
fk <gf0842wf@gmail.com>
2014-06-24 14:59:52 +0800

    m:user.go

--- src/net/ttcp/types/user.go
+++ src/net/ttcp/types/user.go
@@ -15,12 +15,13 @@ type User struct {
 	IsDummy  bool
 	KickOut  bool // 被踢标志
 	IsActive bool // 是否在线
+	IsPrep   bool // 是否准备
 	InGaming bool // 是否游戏中
 	Logined  bool // 是否登陆
 
 	GameType int32  // 游戏类型,eg.100:斗地主
 	RoomID   int32  // 在哪房间, RoomID映射到RoomInfo
-	DeskNo   int32  // 所在桌号
+	DeskNo   int    // 所在桌号,-1表示没有游戏中
 	GameID   uint32 // 正在玩的游戏的唯一编号(自增)
 
 	MQ chan IPCObj // User之间通信队列
