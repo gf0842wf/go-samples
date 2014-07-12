913cb47ca9cac92cc1d36998d1a0909e27a5f969
gf0842wf <gf0842wf@gmail.com>
2014-07-12 10:46:34 +0800

    add send & recv callback
    Signed-off-by: gf0842wf <gf0842wf@gmail.com>

--- src/net/tcpserver/endpoint.go
+++ src/net/tcpserver/endpoint.go
@@ -16,6 +16,8 @@ import (
 1.使用 PutData 发送
 2.读取 RecvBox 管道处理收到的消息
 3.设置 OnConnectionLost 回调函数来处理断开连接, 可以为nil
+4.设置 RawRecv 回调函数来处理解析收到的消息, 可以为nil
+5.设置 RawSend 回调函数来处理封包发送的消息, 可以为nil
 */
 
 type EndPoint struct {
@@ -28,18 +30,33 @@ type EndPoint struct {
 	Heartbeat int64 // 心跳超时(s), < 0表示不设置心跳
 
 	OnConnectionLost interface{} // func(err error)  回调, err: 断开错误信息
+	RawRecv          interface{} // func() (n int, err error) 回调
+	RawSend          interface{} // func([]byte) 回调
 }
 
-func (ep *EndPoint) Init(conn *net.TCPConn, heartbeat int64, sendBufSize int, recvBufSize int, OnConnectionLost interface{}) {
+func (ep *EndPoint) Init(conn *net.TCPConn, heartbeat int64, sendBufSize int, recvBufSize int, OnConnectionLost interface{}, RawRecv interface{}, RawSend interface{}) {
 	ep.Conn = conn
 	ep.Heartbeat = heartbeat
 	ep.Ctrl = make(chan bool)
 	ep.SendBox = make(chan []byte, sendBufSize)
 	ep.RecvBox = make(chan []byte, recvBufSize)
+
 	if OnConnectionLost == nil {
 		ep.OnConnectionLost = ep.onConnectionLost
 	} else {
 		ep.OnConnectionLost = OnConnectionLost
+	}
+
+	if RawRecv == nil {
+		ep.RawRecv = ep.rawRecv
+	} else {
+		ep.RawRecv = RawRecv
+	}
+
+	if RawSend == nil {
+		ep.RawSend = ep.rawSend
+	} else {
+		ep.RawSend = RawSend
 	}
 }
 
@@ -48,15 +65,13 @@ func (ep *EndPoint) PutData(data []byte) {
 }
 
 func (ep *EndPoint) recvData() {
-	header := make([]byte, 4)
-
 	var err error
 
 	for {
 		if ep.Heartbeat > 0 {
 			ep.Conn.SetReadDeadline(time.Now().Add(time.Duration(ep.Heartbeat) * time.Second))
 		}
-		_, err = ep.RawRecv(header)
+		_, err = ep.RawRecv.(func() (int, error))()
 		if err != nil {
 			break
 		}
@@ -64,10 +79,10 @@ func (ep *EndPoint) recvData() {
 	ep.OnConnectionLost.(func(err error))(err)
 }
 
-// 如果封包方式不同,需要修改这个函数
-func (ep *EndPoint) RawRecv(header []byte) (n int, err error) {
-	// header
-	// --这个 ReadFull 非常好用, 作用是一直等到读取header大小的字节数为止
+// 如果封包方式不同,需要修改这个函数,或者通过外部传入回调的方式
+func (ep *EndPoint) rawRecv() (n int, err error) {
+	// header: 本来应该在全局定义来减少分配的, 但是为了封装需要, 在这里定义
+	header := make([]byte, 4)
 	n, err = io.ReadFull(ep.Conn, header)
 	if err != nil {
 		err = errors.New("[EP] Error recv header:" + strconv.Itoa(n) + ":" + err.Error())
@@ -97,17 +112,16 @@ func (ep *EndPoint) onConnectionLost(err error) {
 }
 
 func (ep *EndPoint) sendData() {
-	header := make([]byte, 4)
 	for {
 		select {
 		case data := <-ep.SendBox:
-			ep.RawSend(header, data)
+			ep.RawSend.(func([]byte))(data)
 		case <-ep.Ctrl:
 			defer close(ep.SendBox)
 			defer close(ep.RecvBox)
 			// 准备关闭连接, 要发完剩下的消息
 			for data := range ep.SendBox {
-				ep.RawSend(header, data)
+				ep.RawSend.(func([]byte))(data)
 			}
 			ep.Conn.Close()
 
@@ -118,8 +132,10 @@ func (ep *EndPoint) sendData() {
 	}
 }
 
-// 如果封包方式不同,需要修改这个函数
-func (ep *EndPoint) RawSend(header []byte, msg []byte) {
+// 如果封包方式不同,需要修改这个函数,或者通过外部传入回调的方式
+func (ep *EndPoint) rawSend(msg []byte) {
+	// header: 本来应该在全局定义来减少分配的, 但是为了封装需要, 在这里定义
+	header := make([]byte, 4)
 	length := len(msg)
 	binary.BigEndian.PutUint32(header, uint32(length))
 	data := append(header, msg...)
